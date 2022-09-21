21485e97f7cc13dbabb8ad93959d3fc3a8a3e942
Wang Hang <954872496@qq.com>
2022-09-21 15:01:16 +0800

    add feign

---
+++ cloud_demo/user-service/src/main/java/cn/itcast/user/pojo/PatternProperties.java
@@ -0,0 +1,13 @@
+package cn.itcast.user.pojo;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "pattern")
+public class PatternProperties {
+    private String dataformat;
+    private String envSharedValue;
+}
--- cloud_demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud_demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,18 +1,31 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.pojo.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
 @RequestMapping("/user")
+//@RefreshScope//动态更新Nacos的配置
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    //@Value("${pattern.dataformat}")
+    //private String dataformat;
+
+    @Autowired
+    private PatternProperties patternProperties;
 
     /**
      * 路径： /user/110
@@ -24,4 +37,16 @@ public class UserController {
     public User queryById(@PathVariable("id") Long id) {
         return userService.queryById(id);
     }
+
+    @GetMapping("/now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDataformat()));
+
+    }
+
+    @GetMapping("/prop")
+    public PatternProperties prop(){
+        return patternProperties;
+    }
+
 }
